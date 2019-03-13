878ef1216f4bc2492ff1920be4e584f7849fd92b
dit000 <dit000@dit000.local>
2019-03-13 16:17:49 +0900

    ColorToggle

--- ButtonFun01/ViewController.swift
+++ ButtonFun01/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var mySubView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor=UIColor.green
+        myLabel.backgroundColor=UIColor.red
+        myLabel.text = "Hello iPhone!"
+        print(myLabel.text!)
+        
     }
 
+    @IBAction func buttonPressed(_ sender: Any) {
+        var press = false;
+        print("버튼이 눌러졌다.")
+        if(press == false){
+            press = true
+            myLabel.text = "Button Pressed"
+            mySubView.backgroundColor = UIColor.blue
+        }
+        else if(press == true){
+            press = false
+            myLabel.text = "Button Pressed again"
+            mySubView.backgroundColor = UIColor.red
+        }
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
